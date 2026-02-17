897071e99627bda4437e47f213ffaadbfa02dc5e
tongsiwai <tongsir@gmail.com>
2026-02-17 23:59:12 +1300

    Add Publications section to home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/home/HeroSection";
 import EducationSection from "@/components/home/EducationSection";
 import ExperienceSection from "@/components/home/ExperienceSection";
+import PublicationsSection from "@/components/home/PublicationsSection";
 import SkillsSection from "@/components/home/SkillsSection";
 import BlogSection from "@/components/home/BlogSection";
 import YoutubeSection from "@/components/home/YoutubeSection";
@@ -11,6 +12,7 @@ export default function Index() {
       <HeroSection />
       <EducationSection />
       <ExperienceSection />
+      <PublicationsSection />
       <SkillsSection />
       <BlogSection />
       <YoutubeSection />
